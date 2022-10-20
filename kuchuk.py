bf4f78bf423a35e14c4e4a88490d873c425b0845
Professional <kuchukav1981@gmail.com>
2022-10-21 01:14:28 +0300

    изменения

---
+++ kuchuk.py
@@ -0,0 +1,16 @@
+# from random import randint
+#
+# n_chisel = int(input('Введите количество случайных чисел списка: '))
+#
+# def average_of_numbers(spisok):
+#
+#     s = sum(spisok)
+#     return s//n_chisel
+#
+# spisok = [randint(0, 1000) for i in range(n_chisel)]
+#
+# print(f'Список случайных чисел: {spisok}')
+# print(f'Среднее значение чисел списка равно: {average_of_numbers(spisok)}')
+#
+#
+# # домашнее задание
